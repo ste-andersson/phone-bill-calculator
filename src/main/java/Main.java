566fb95704628532c2a4556588f7ad72766bd712
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-17 13:17:28 +0100

    Add user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,29 @@ public class Main {
 
     public static void main(String args[]) {
 
-        scanner.close()
+        double baseCost = getBaseCost();
+        int overageMinutes = getOverageMinutes();
 
+        scanner.close();
+
+    }
+
+    public static double getBaseCost() {
+        double baseCost;
+        do {
+            System.out.println("Enter base cost of the plan:");
+            baseCost = scanner.nextDouble();
+        } while (baseCost < 0);
+        return baseCost;
+    }
+
+    public static int getOverageMinutes() {
+        int overageMinutes;
+        do {
+            System.out.println("Enter overage minutes:");
+            overageMinutes = scanner.nextInt();
+        } while(overageMinutes < 0);
+        return overageMinutes;
     }
 
 }
