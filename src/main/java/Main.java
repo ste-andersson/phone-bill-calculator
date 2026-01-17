04da7f069394e059814fb5791273cf9196acc4fc
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-17 12:55:38 +0100

    Add known information

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static double taxRate = 0.15;
+    static double overageFee = 0.25;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+        scanner.close()
+
+    }
+
+}
